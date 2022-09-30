d876bdf618422f260cba05ffa5d885f9138428bb
Alexander Visheratin <alexvish91@gmail.com>
2022-09-30 16:24:14 -0400

    Added augmentation.

--- grammar-check/dataset.py
+++ grammar-check/dataset.py
@@ -1,7 +1,11 @@
 import csv
 import itertools
 import os
+import random
 
+import nlpaug.augmenter.char as nac
+import nlpaug.augmenter.word as naw
+import nlpaug.flow as flow
 import torch.utils.data
 from transformers import AutoTokenizer
 
@@ -23,6 +27,15 @@ class Dataset(torch.utils.data.IterableDataset):
         for i in range(rank, len(files), world_size):
             file_path = os.path.join(data_dir, files[i])
             self.files.append(file_path)
+        self.aug_methods = [
+            nac.OcrAug(aug_char_max=2, aug_word_max=2),
+            nac.KeyboardAug(aug_char_max=2, aug_word_max=2),
+            nac.RandomCharAug(action="insert", aug_char_max=2, aug_word_max=2),
+            nac.RandomCharAug(action="substitute", aug_char_max=2, aug_word_max=2),
+            nac.RandomCharAug(action="swap", aug_char_max=2, aug_word_max=2),
+            nac.RandomCharAug(action="delete", aug_char_max=2, aug_word_max=2),
+            naw.SpellingAug(aug_max=3),
+        ]
 
     def __iter__(self):
         worker_total_num = torch.utils.data.get_worker_info().num_workers
@@ -33,17 +46,21 @@ class Dataset(torch.utils.data.IterableDataset):
             reader = csv.reader(
                 (x.replace("\0", "") for x in file_handler), delimiter="\t"
             )
-            iter_reader = itertools.islice(reader, worker_id, None, worker_total_num)
+            mapped_iter = map(self.row_process, reader)
+            iter_reader = itertools.islice(
+                mapped_iter, worker_id, None, worker_total_num
+            )
             readers.append(iter_reader)
         group_iter = itertools.chain.from_iterable(readers)
-        mapped_iter = map(self.row_process, group_iter)
-        return mapped_iter
+        return group_iter
 
     def row_process(self, row):
         if len(row) == 1:
             row.append(row[0])
+        aug_method_idx = random.randrange(0, len(self.aug_methods))
+        aug_input = self.aug_methods[aug_method_idx].augment(row[0])
         input_tokens = self.tokenizer(
-            row[0],
+            aug_input[0],
             max_length=MAX_LENGTH,
             add_special_tokens=True,
             truncation=True,
